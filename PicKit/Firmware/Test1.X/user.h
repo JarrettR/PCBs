c4510ed96a97c5beb326fa347e0c1ae6e9dfa3d8
JarrettR <jrainier@gmail.com>
2016-04-06 22:21:37 -0700

    Started testing PicKit (finally!)

---
+++ PicKit/Firmware/Test1.X/user.h
@@ -0,0 +1,14 @@
+/******************************************************************************/
+/* User Level #define Macros                                                  */
+/******************************************************************************/
+
+
+#define LED_1       PORTCbits.RC0
+#define LED_1_TRI	TRISCbits.TRISC0
+/******************************************************************************/
+/* User Function Prototypes                                                   */
+/******************************************************************************/
+
+/* TODO User level functions prototypes (i.e. InitApp) go here */
+
+void InitApp(void);         /* I/O and Peripheral Initialization */
